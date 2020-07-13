4bd5258f0622dab3f8d87bcb162b897394639ada
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2020-07-13 17:27:00 -0300

    Ejercicio del 13 del 7

--- EjerciciosLaboratorio/src/servicios/compra/RegistroDeDatos.java
+++ EjerciciosLaboratorio/src/servicios/compra/RegistroDeDatos.java
@@ -1,23 +1,91 @@
 package servicios.compra;
 
-import java.util.Scanner;
+import seresVivos.Persona;
 
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*PANTALLA COMPLETA */
 public class RegistroDeDatos {
     public static void main(String[] args) {
 
-        Scanner ingresoDatos = new Scanner(System.in);
-        System.out.println("Buen día, por favor ingrese su nombre");
-        String nombre = ingresoDatos.nextLine();
-        System.out.println("Ingrese su apellido");
-        String apellido = ingresoDatos.nextLine();
-        System.out.println("Hola: " + nombre + " " +apellido);
-        System.out.println("...................................................");
-        System.out.println("Ingrese su edad");
-        int Edad = ingresoDatos.nextInt();
-        System.out.println(Edad);
-        System.out.println("Ingrese su ocupacion");
-        ingresoDatos.nextLine();
-        String Ocupacion= ingresoDatos.nextLine();
-        System.out.println(Ocupacion);
+        ArrayList <Persona> Personas= new ArrayList<>();
+
+        JFrame ventana= new JFrame();
+        ventana.setLayout(null);
+        ventana.setSize(500,700);
+        ventana.setVisible(true);
+
+        JLabel nombre= new JLabel();
+        nombre.setText("Ingrese nombre");
+        nombre.setLocation(200,25);
+        nombre.setSize(100,100);
+        ventana.add(nombre);
+
+        JTextField campoNombre= new JTextField();
+        campoNombre.setLocation(150,100);
+        campoNombre.setSize(200,25);
+        ventana.add(campoNombre);
+
+        JLabel apellido= new JLabel();
+        apellido.setText("Ingrese apellido");
+        apellido.setLocation(200,100);
+        apellido.setSize(100,100);
+        ventana.add(apellido);
+
+
+        JTextField campoApellido= new JTextField();
+        campoApellido.setLocation(150,175);
+        campoApellido.setSize(200,25);
+        ventana.add(campoApellido);
+
+        JLabel edad= new JLabel();
+        edad.setText("Ingrese edad");
+        edad.setLocation(200,175);
+        edad.setSize(100,100);
+        ventana.add(edad);
+
+        JTextField campoEdad= new JTextField();
+        campoEdad.setLocation(150,250);
+        campoEdad.setSize(200,25);
+        ventana.add(campoEdad);
+
+        JLabel dni= new JLabel();
+        edad.setText("Ingrese dni");
+        edad.setLocation(200,250);
+        edad.setSize(100,100);
+        ventana.add(edad);
+
+        JTextField campoDni= new JTextField();
+        campoDni.setLocation(150,325);
+        campoDni.setSize(200,25);
+        ventana.add(campoDni);
+
+        JButton boton= new JButton("Guardar Informacion");
+        boton.setSize(200,25);
+        boton.setLocation(150,400);
+        ventana.add(boton);
+
+        JLabel confirmacion= new JLabel();
+        confirmacion.setLocation(200,450);
+        confirmacion.setSize(100,100);
+        ventana.add(confirmacion);
+
+        boton.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String nombrePersona=campoNombre.getText();
+                String apellidoPersona=campoApellido.getText();
+                String edadPersona=campoEdad.getText();
+                String dniPersona=campoDni.getText();
+                confirmacion.setText("Registrado correctamente");
+
+            }
+
+        });
+
     }
 }
