297946e654c1c4324fbe7e84a08fb637481c65a9
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2020-09-09 12:17:34 -0300

    2)
    Profe, no se puede hacer,  ya que no hay precio, saludos

---
+++ EjerciciosLaboratorio/src/RestauranteGUI/EmprendimientoGastronomico.java
@@ -0,0 +1,8 @@
+package RestauranteGUI;
+
+public interface EmprendimientoGastronomico {
+
+    int obtenerIngresoTotal();
+
+    //despues usar el implents EmprendimientoGastronomico donde tiene que estar
+}
